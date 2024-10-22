729e21c4fe6a7e8557da3d7a9ccb6e89c7b83a60
Abhishek Raghuwanshi <abhishek.raghuwanshi@indxtechnology.com>
2024-10-22 17:23:56 +0530

    Implemented OpenAPI specification and Validation.

--- server/routes/employeeRoute.js
+++ server/routes/employeeRoute.js
@@ -5,8 +5,8 @@ import { userAuthentication } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 router.use(userAuthentication(['employee']));
 
-router.post('/', createLeaveRequest);
-router.get('/', getLeaveRequests);
+router.post('/leaves', createLeaveRequest);
+router.get('/leaves', getLeaveRequests);
 router.patch('/leaves/:leave_id', modifyLeaveRequest);
 router.delete('/leaves/:leave_id', deleteLeaveRequest);
 
--- server/server.js
+++ server/server.js
@@ -5,11 +5,20 @@ import authRoute from "./routes/authRoute.js";
 import adminRoute from "./routes/adminRoute.js";
 import employeeRoute from "./routes/employeeRoute.js";
 import User from "./models/user.js";
-import Leave from "./models/leave.js"
+import Leave from "./models/leave.js";
 import dotenv from 'dotenv';
+import * as OpenApiValidator from 'express-openapi-validator';
+import swaggerUi from 'swagger-ui-express';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import YAML from 'yamljs'; 
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Set up associations (one-to-many association)
 User.hasMany(Leave, { foreignKey: 'e_id' });
 Leave.belongsTo(User, { foreignKey: 'e_id' });
 
@@ -21,12 +30,33 @@ app.use(cors({
     credentials: true,
 }));
 
+// Load and serve Swagger UI
+const openAPIFilePath = path.join(__dirname, 'openapi.yml'); 
+const swaggerDocument = YAML.load(openAPIFilePath); 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// OpenAPI validation middleware
+app.use(
+    OpenApiValidator.middleware({
+        apiSpec: openAPIFilePath, // Path to your OpenAPI spec
+        validateRequests: true, // Validate requests against the OpenAPI spec
+        validateResponses: false, // Validate responses against the OpenAPI spec (false by default)
+    })
+);
+
 app.use('/api/auth', authRoute);
 app.use('/api/admin', adminRoute);
 app.use('/api/employees', employeeRoute);
 
+// Error handling middleware for OpenAPI validation errors
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      message: err.message,
+      errors: err.errors,
+    });
+  });
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
 sequelize.sync({ force: false })
     .then(() => {
